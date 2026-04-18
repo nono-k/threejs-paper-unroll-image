47e43b775300acf8a1810c38b81d2256b189c5ff
iwasaki <poettype73@gmail.com>
2026-04-18 09:48:59 +0900

    Fix

--- src/scripts/shaders/fragment.glsl
+++ src/scripts/shaders/fragment.glsl
@@ -8,13 +8,10 @@ uniform float uProgress;
 
 void main() {
   vec4 textureColor = texture2D(uTexture, vUv);
-  // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-
   vec3 color;
 
   color = textureColor.rgb * vFrontShadow;
   float alpha = clamp(uProgress * 5.0, 0.0, 1.0);
-  // float alpha = 1.0;
 
   gl_FragColor = vec4(color, alpha);
 }
--- src/scripts/shaders/vertex.glsl
+++ src/scripts/shaders/vertex.glsl
@@ -48,9 +48,16 @@ void main() {
   float rad = 0.1; // 半径
   float rolls = 8.0; // ロールの巻き回数
 
-  newPosition = rotate(newPosition - vec3(-0.5, 0.5, 0.0), vec3(0.0, 0.0, 1.0), - finalAngle) + vec3(-0.5, 0.5, 0.0);
+  // 座標系を回転させる
+  newPosition = rotate(
+    newPosition - vec3(-0.5, 0.5, 0.0),
+    vec3(0.0, 0.0, 1.0),
+    - finalAngle
+  ) + vec3(-0.5, 0.5, 0.0);
 
+  // -0.5 ~ 0.5 -> 0.0 ~ 1.0
   float offs = (newPosition.x + 0.5) / (sin(finalAngle) + cos(finalAngle));
+  // 切り替え係数(tProgress)を計算する
   float tProgress = clamp((uProgress - offs * 0.99) / 0.01, 0.0, 1.0);
 
   vFrontShadow = clamp((uProgress - offs * 0.95) / 0.05, 0.7, 1.0);
@@ -58,16 +65,29 @@ void main() {
   newPosition.z = rad + rad * (1.0 - offs / 2.0) * sin(-offs * rolls * PI - 0.5 * PI);
   newPosition.x = -0.5 + rad * (1.0 - offs / 2.0) * cos(-offs * rolls * PI + 0.5 * PI);
 
-  newPosition = rotate(newPosition - vec3(-0.5, 0.5, 0.0), vec3(0.0, 0.0, 1.0),finalAngle) + vec3(-0.5, 0.5, 0.0);
+  // 元の角度に戻す
+  newPosition = rotate(
+    newPosition - vec3(-0.5, 0.5, 0.0),
+    vec3(0.0, 0.0, 1.0),
+    finalAngle
+  ) + vec3(-0.5, 0.5, 0.0);
 
-  newPosition = rotate(newPosition - vec3(-0.5, 0.5, rad), vec3(sin(finalAngle), cos(finalAngle), 0.0), -PI * uProgress * rolls);
+  // 巻き回転(立体化)
+  newPosition = rotate(
+    newPosition - vec3(-0.5, 0.5, rad),
+    vec3(sin(finalAngle), cos(finalAngle), 0.0),
+    -PI * uProgress * rolls
+  );
 
+  // 位置補正
   newPosition += vec3(
     -0.5 + uProgress * cos(finalAngle) * (sin(finalAngle) + cos(finalAngle)),
     0.5 - uProgress * sin(finalAngle) * (sin(finalAngle) + cos(finalAngle)),
     rad * (1.0 - uProgress / 2.0)
   );
 
+  // 元の形との補間
   vec3 finalposition = mix(newPosition, position, tProgress);
+
   gl_Position = projectionMatrix * modelViewMatrix * vec4(finalposition, 1.0 );
 }
